2bbc95d5f64dafce438549ea7be754c99103cc94
Jon Laing <jon.dreaux.laing@gmail.com>
2011-11-11 16:50:33 -0500

    Basic scaffold of Proposals controller

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -3,4 +3,33 @@ class ProposalsController < ApplicationController
     @popular = Proposal.most_popular(10)
     @recent = Propsal.limit(10)
   end
+
+  def show
+    @proposal = Proposal.find(params[:id])
+  end
+
+  def new
+    @proposal = Proposal.new
+  end
+
+  def create
+    @proposal = Proposal.create!(params)
+  rescue ActiveRecord::RecordInvalid
+    render :action => :new
+  end
+
+  def edit
+    @proposal = Proposal.find(params[:id])
+  end
+
+  def update
+    @proposal = Proposal.find(params[:id])
+    @proposal.body = params[:body]
+    @proposal.save
+  end
+
+  def destroy
+    @proposal = Proposal.find(params[:id])
+    @proposal.destroy
+  end
 end
