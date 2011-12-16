491984f412ad07aebd1805ec2af2e97f1c1422fa
jonlaing <jon.dreaux.laing@gmail.com>
2011-12-15 22:59:56 -0500

    Trying to get a pass on Travis... not sure why it isn't working

--- app/models/assembly_category.rb
+++
@@ -1,3 +0,0 @@
-class AssemblyCategory < ActiveRecord::Base
-  belongs_to :assembly, :foreign_key => :uuid
-end
--- spec/models/assembly_category_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe AssemblyCategory do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
