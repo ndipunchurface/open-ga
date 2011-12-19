31000c087ada040fc33867d216669aedbb9c5b7e
Jon Laing <jon.dreaux.laing@gmail.com>
2011-12-19 15:50:50 -0500

    Added Compass and Blueprint

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'devise'
 # in production environments by default.
 group :assets do
   gem 'sass-rails',   '~> 3.1.4'
+  gem 'compass', '~> 0.12.alpha'
   gem 'coffee-rails', '~> 3.1.1'
   gem 'uglifier', '>= 1.0.3'
   gem 'slim'
--- config/application.rb
+++ config/application.rb
@@ -48,6 +48,8 @@ module RemoteVote
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.precompile << /(^[^_]|\/[^_])[^\/]*$/
+
     # For deployment on Heroku
     config.assets.initialize_on_precompile = false
   end
